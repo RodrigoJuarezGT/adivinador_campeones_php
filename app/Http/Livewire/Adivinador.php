c4c376316417cfca0889a6055205776d7cf13498
Rodrigo Juarez <rodrigo.arturojuarez@gmail.com>
2022-04-22 12:44:46 -0600

    se finaliza adivinador basico

--- app/Http/Livewire/Adivinador.php
+++ app/Http/Livewire/Adivinador.php
@@ -14,38 +14,47 @@ class Adivinador extends Component
         $array_answers = answers::where('id', 1)->get();
         $answers = $array_answers[0];
         $question = 'Pais';
+        $player_name = '';
+        $guessed = false;
 
-        if($request->answer && !$answers->country){
-            $answers->country = $request->answer;
-            $question = 'Club';
-        }elseif ($request->answer && !$answers->club) {
-            $answers->club = $request->answer;
-            $question = 'Edad';
-        }elseif($request->answer && !$answers->age){
-            $answers->age = $request->answer;
-            $question = 'Posicion';
-        }elseif($request->answer && !$answers->position){
-            $answers->position = $request->answer;
-            $question = 'Tu jugador es!';
+        if($request->answer == 'si'){
+            $guessed = true;
+        }else{
+            if($request->answer && !$answers->country){
+                $answers->country = $request->answer;
+                $question = 'Club';
+            }elseif ($request->answer && !$answers->club) {
+                $answers->club = $request->answer;
+                $question = 'Edad';
+            }elseif($request->answer && !$answers->age){
+                $answers->age = $request->answer;
+                $question = 'Posicion';
+            }elseif($request->answer && !$answers->position){
+                $answers->position = $request->answer;
+                $question = 'Es este el nombre';
 
-            $player = players::where('country', $answers->country)->
-                where('age', $answers->age)->
-                where('club', $answers->club)->
-                where('position', $answers->position)->get();
+                $player = players::where('country', $answers->country)->
+                    where('age', $answers->age)->
+                    where('club', $answers->club)->
+                    where('position', $answers->position)->get();
 
-            // dd($player);
-            $request->answer = null;
-            $answers->country = null;
-            $answers->club = null;
-            $answers->age = null;
-            $answers->position = null;
+                $player_name = $player[0]->name;
+
+                // dd($player);
+                $answers->country = null;
+                $answers->club = null;
+                $answers->age = null;
+                $answers->position = null;
+            }
+            $answers->save();
         }
 
-        $answers->save();
 
         return view('livewire.adivinador', [
             'answers' => $answers[0],
             'question' => $question,
+            'player_name' => $player_name,
+            'guessed' => $guessed,
             ]);
     }
 }
--- resources/views/livewire/adivinador.blade.php
+++ resources/views/livewire/adivinador.blade.php
@@ -1,3 +1,11 @@
 <div>
-¿{{ $question }} del jugador?
+@if($guessed)
+<h1>GAME OVER</h1>
+<h2>Estabas pensando en {{ $player_name }}</h2>
+@else
+    ¿{{ $question }} del jugador?
+    @if($player_name)
+    <h1>{{ $player_name }}</h1>
+    @endif
+@endif
 </div>
